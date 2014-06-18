a1e1c46b89cef7cc9d5d8be7a3d6e2b1cfb0b502
rachellogie <rachel.logie1@gmail.com>
2014-06-18 11:35:25 -0600

    refactored the count not in decline method

--- lib/count_grading.rb
+++ lib/count_grading.rb
@@ -18,13 +18,6 @@ class CountGrading
   end
 
   def count_not_in_decline
-    array =  @data.map {|each| each[1]}
-
-    count = 0
-    array.each do |each|
-      grader = Grading.new(each)
-      count += 1 if !grader.in_decline?
-    end
-    count
+    @data.length - count_in_decline
   end
 end
